a5062c82622be9e8547a15710f3bdd59c3ea3585
Natãn Moroni <natan.moroni@gmail.com>
2023-11-30 21:07:12 -0300

    trabalhando no JSONserver

--- codigo/assets/JS/home_pos_login.js
+++ codigo/assets/JS/home_pos_login.js
@@ -1,23 +1,68 @@
-/************************* Recuperando os dados das partidas *************************/
+/************************* Métodos os dados das partidas *************************/
 const urlPartidas =
-  "https://jsonserver-partidas.nayarissonnatan.repl.co/partidas";
+  "https://jsonserver-partidas-1.nayarissonnatan.repl.co/partidas";
 var partidas = [];
 
-function LeDadosJSONServer() {
-  fetch(urlPartidas)
-    .then(function (response) {
-      return response.json();
-    })
-    .then((data) => {
-      partidas = data;
-      console.log("dados carregados!" + partidas[1].Criador);
-    });
-}
+async function LeDadosJSONServer() {
 
-//.then (function(dados) {
-//
-//
-//})
+  const req = await fetch(urlPartidas)
+  const partidasJson = await req.json()
+
+  partidas = partidasJson;
+  // fetch(urlPartidas)
+  //   .then(function (response) {
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     partidas = data;
+  //     console.log("dados carregados!" + partidas[1].Criador);
+  //   });
+
+}
+LeDadosJSONServer()
+
+
+/************************* Atualizando a lotacao das partidas *************************/
+
+async function AtualizaLotacao(id) {
+  await LeDadosJSONServer()
+  let somaLotacao = partidas[id].lotacao; 
+
+  console.log(somaLotacao);
+
+  var altera = {
+    id: id,
+    Criador: partidas[id].Criador,
+    Esporte: partidas[id].Esporte,
+    Data: partidas[id].Data,
+    Horario: partidas[id].Horario,
+    Jogadores: partidas[id].Jogadores,
+    Categoria: partidas[id].Categoria,
+    Obrigatorio: partidas[id].Obrigatorio,
+    lotacao: somaLotacao += 1
+  };
+  fetch(`${urlPartidas}/${id}`, {
+      method: 'PUT',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(altera)
+  })
+      .then(response => {
+        if(!response.ok){
+          console.log("err"); 
+          return;
+        }
+        response.json();
+      })
+      
+          .then(data => {
+          console.log("Contato alterado com sucesso");
+      })
+      .catch(error => {
+          console.log('Erro ao atualizar contato via API JSONServer');
+      });
+}
 
 /************************* Programando a abertura do pop-up para cada partida *************************/
 
@@ -30,7 +75,7 @@ Array.from(openPopupButtons).forEach(function (button) {
     // Obtém o id do elemento clicado
     elementId = button.id;
 
-    ValidaInfo();
+    ValidaInfoID();
   });
 });
 
@@ -43,107 +88,19 @@ var pJogadores = document.getElementById("p-jogadores");
 var pCategoria = document.getElementById("p-categoria");
 var pObrigatorio = document.getElementById("p-obrigatorio");
 
-/* var dados = [
-  {
-    id: 0,
-    Criador: "Alex Justos",
-    Esporte: "Futsal",
-    Data: "18/11/2024",
-    Horário: "18h",
-    Jogadores: "/12",
-    Categoria: "Masculina",
-    Obrigatório: "chuteira",
-    Jogado:[{
-      id1: true},
-      {id2: true},
-      {id3: true}]
-  },
-  {
-    id: 1,
-    Criador: "Maria Silva",
-    Esporte: "Futebol",
-    Data: "20/11/2024",
-    Horário: "19h",
-    Jogadores: "/10",
-    Categoria: "Feminina",
-    Obrigatório: "caneleira",
-  },
-  {
-    id: 2,
-    Criador: "João Santos",
-    Esporte: "Futsal",
-    Data: "22/11/2024",
-    Horário: "20h",
-    Jogadores: "/8",
-    Categoria: "Masculina",
-    Obrigatório: "tênis",
-  },
-  {
-    id: 3,
-    Criador: "Ana Oliveira",
-    Esporte: "Futebol",
-    Data: "24/11/2024",
-    Horário: "17h",
-    Jogadores: "/14",
-    Categoria: "Feminina",
-    Obrigatório: "chuteira",
-  },
-  {
-    id: 4,
-    Criador: "Pedro Lima",
-    Esporte: "Futsal",
-    Data: "26/11/2024",
-    Horário: "21h",
-    Jogadores: "/16",
-    Categoria: "Masculina",
-    Obrigatório: "chuteira",
-  },
-  {
-    id: 5,
-    Criador: "Mariana Costa",
-    Esporte: "Futebol",
-    Data: "28/11/2024",
-    Horário: "16h",
-    Jogadores: "/6",
-    Categoria: "Feminina",
-    Obrigatório: "tênis",
-  },
-  {
-    id: 6,
-    Criador: "Carlos Mendes",
-    Esporte: "Futsal",
-    Data: "30/11/2024",
-    Horário: "19h",
-    Jogadores: "/4",
-    Categoria: "Masculina",
-    Obrigatório: "caneleira",
-  },
-  {
-    id: 7,
-    Criador: "Isabela Oliveira",
-    Esporte: "Futebol",
-    Data: "02/12/2024",
-    Horário: "22h",
-    Jogadores: "/9",
-    Categoria: "Feminina",
-    Obrigatório: "tênis",
-  },
-];
-
-*/
 
 //função que busca nos dados estruturados a respectiva partida que o usuário deseja
-function ValidaInfo() {
+function ValidaInfoID() {
   let procura = partidas.length;
   for (let i = 0; i < procura; i++) {
     if (partidas[i].id == elementId) {
       pCriador.textContent = "Criador: " + partidas[i].Criador;
       pEsporte.textContent = "Esporte: " + partidas[i].Esporte;
       pData.textContent = "Data: " + partidas[i].Data;
-      pHorario.textContent = "Horário: " + partidas[i].Horário;
+      pHorario.textContent = "Horário: " + partidas[i].Horario;
       pJogadores.textContent = "Jogadores: " + partidas[i].Jogadores;
       pCategoria.textContent = "Categoria: " + partidas[i].Categoria;
-      pObrigatorio.textContent = "Obrigatório: " + partidas[i].Obrigatório;
+      pObrigatorio.textContent = "Obrigatório: " + partidas[i].Obrigatorio;
       procura = i;
     }
   }
@@ -172,7 +129,7 @@ const appendAlert = (message, type) => {
   ].join("");
 
   alertPlaceholder.append(wrapper);
-};
+}
 
 /*   implementar após a junção do código do trablho
 
@@ -197,14 +154,7 @@ let mensagemExito =
 // let mensagemFalha = "Ops, parece que a partida já está cheia!";
 
 alertTrigger.addEventListener("click", () => {
+  AtualizaLotacao(elementId)
   appendAlert(mensagemExito, "success");
 });
 //}
-//função para fazer a mensagem desaparecer
-console.log(dados[0].Jogado[0]);
-/* 
-setTimeout(function () {
-  notificacao.className = 'alert alert-success alert-dismissible closed.bs.alert';
-}, 1000);
-
-*/
