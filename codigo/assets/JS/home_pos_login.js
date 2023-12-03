f173032849611e949194a761d67f2c0dea366058
Natãn Moroni <natan.moroni@gmail.com>
2023-12-03 17:56:33 -0300

    Funcionalidade de clicar no botão de participar e acrescentar a partida para a coleção do usuario está finalizada, o dado da partida é acrescentado ao JSONServer quando o botão é clicado e não permite duplicação de partidas

--- codigo/assets/JS/home_pos_login.js
+++ codigo/assets/JS/home_pos_login.js
@@ -3,7 +3,7 @@ const urlPartidas =
   "https://jsonserver-partidas-1.nayarissonnatan.repl.co/partidas";
 var partidas = [];
 
-async function LeDadosJSONServer() {
+async function LeDadosPartidas() {
 
   const req = await fetch(urlPartidas)
   const partidasJson = await req.json()
@@ -19,13 +19,27 @@ async function LeDadosJSONServer() {
   //   });
 
 }
-LeDadosJSONServer()
+LeDadosPartidas()
+
+//Obtendo os usuários cadastrados no site pelo JSONServer
+const urlUsuarios = "https://jsonserver-partidas-1.nayarissonnatan.repl.co/usuarios";
+var usuarios = [];
+
+async function LeDadosUsuarios() {
+
+  const res = await fetch(urlUsuarios)
+  const usuariosJson = await res.json()
+
+  usuarios = usuariosJson;
+
+}
+LeDadosUsuarios()
 
 
 /************************* Atualizando a lotacao das partidas *************************/
 
 async function AtualizaLotacao(id) {
-  await LeDadosJSONServer()
+  await LeDadosPartidas()
   let somaLotacao = partidas[id].lotacao;
   console.log(partidas[id].Horario)
 
@@ -56,12 +70,69 @@ async function AtualizaLotacao(id) {
       })
       
           .then(data => {
-          console.log("Contato alterado com sucesso");
+          console.log("Lotação alterada com sucesso");
       })
       .catch(error => {
           console.log('Erro ao atualizar contato via API JSONServer');
       });
       console.log(somaLotacao)
+}
+
+//atualiza as partidas que o usuário está participando
+var atualizapartidas = [];
+
+async function AtualizaPartidasUsuario(IdUsuario, IdPartida) {
+  await LeDadosUsuarios();
+  let indice = IdUsuario - 1;
+  let tamvetor = usuarios[indice].partidas.length;
+  let jaParticipa = false;
+
+  for(let i = 0; i <= tamvetor; i++){
+      if(i < tamvetor){
+    atualizapartidas[i] = usuarios[indice].partidas[i];
+          
+        if(usuarios[indice].partidas[i] == parseInt(IdPartida, 10))
+        {
+          jaParticipa = true;
+        }
+      }
+      
+      else if(i === tamvetor && !jaParticipa){
+      atualizapartidas[i] = parseInt(IdPartida, 10);
+    }
+   
+}
+
+console.log(atualizapartidas)
+  
+var alteraPartidas = {
+    id: IdUsuario,
+    login: usuarios[indice].login,
+    senha: usuarios[indice].senha,
+    nome: usuarios[indice].nome,
+    email: usuarios[indice].email,
+    partidas: atualizapartidas
+  };
+  fetch(`${urlUsuarios}/${IdUsuario}`, {
+      method: 'PUT',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(alteraPartidas)
+  })
+      .then(response => {
+        if(!response.ok){
+          console.log("err"); 
+          return;
+        }
+        return response.json();
+      })
+          .then(data => {
+          console.log("Usuário participando com sucesso");
+      })
+      .catch(error => {
+          console.log('Erro ao atualizar contato via API JSONServer');
+      });
 }
 
 /************************* Programando a abertura do pop-up para cada partida *************************/
@@ -106,13 +177,46 @@ function ValidaInfoID() {
   }
 }
 
+//descobrindo quem está acessando o site para guardar as partidas que ele está participando no JSONServer
+var objdado = "";
+function LeLocalStorage(){
+  let strdado = localStorage.getItem('db');
+
+  if(strdado){
+  objdado = strdado;
+  }
+  else{
+    //teste, retirar esse valor para objdado depois 
+    objdado = "admin@abc.com";
+    //console.log("o dado padrão para teste foi carregado " + objdado.usuario[0].email)
+  }
+  return objdado;
+}
+LeLocalStorage();
+
+//descobrindo o id de usuário pelo seu email
+var NovoParticipando; 
+async function ProcuraIdUsuario() {
+  await LeDadosUsuarios();
+  let procura = usuarios.length;
+  let usuarioEncontrado = false;
+  for (let i = 0; i < procura; i++) {
+
+    if (usuarios[i].email == objdado) {
+      console.log("usuário encontrado, id: " + usuarios[i].id);
+      usuarioEncontrado = true;
+      NovoParticipando = usuarios[i].id;
+    }
+  }
+
+  if(!usuarioEncontrado) {
+    console.log("Usuário não encontrado");
+  }
+
+  return NovoParticipando;
+}
 /*********************************** Programando o botão de participar ***********************************/
-//exemplo teste: buscar o id do usuário para cadastrar ele na partida
-//Obs.: o id deve ser guardado no localstorage a partir da tela de login
 
-//fim do teste
-
-//puxa o id do usuário
 
 /*********************************** Programando a Notificação ***********************************/
 
@@ -154,7 +258,8 @@ let mensagemExito =
 // let mensagemFalha = "Ops, parece que a partida já está cheia!";
 
 alertTrigger.addEventListener("click", () => {
-  AtualizaLotacao(elementId)
+  AtualizaPartidasUsuario(1, elementId)
+  AtualizaLotacao(elementId);
   appendAlert(mensagemExito, "success");
 });
 //}
