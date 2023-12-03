c8ceb6adbdc4e04fe0b9cceb5efeb93617de1916
Neguim10 <gabrieljordann@hotmail.com>
2023-12-03 02:35:39 -0300

    Coloquei o Chat na home pós login
    Preciso arrumar como ele abre e fecha apenas

---
+++ codigo/assets/JS/chat.js
@@ -0,0 +1,68 @@
+
+// sendImage.addEventListener("click", () => renderUserMessage()); //
+
+
+
+ /* txtInput.addEventListener("keyup", (event) => {
+        if (event.key === 'Enter') {
+                renderUserMessage();
+        }
+}); */
+const chatHeader = document.querySelector(".chat-header")
+const container = document.querySelector(".container")
+function handleKeyPress(event) {
+        if (event.key === 'Enter') {
+        renderUserMessage();
+        }
+    }
+
+    chatHeader.addEventListener("click", () => {
+        container.classList.toggle("colapsar");
+    });
+    
+const renderUserMessage = () => {
+        
+        const txtInput = document.getElementById('txtInput');
+       
+
+        const Userinput = txtInput.value;
+        renderMessageElement(Userinput, "user")
+        txtInput.value = "";
+        setTimeout(()=>{
+                renderChatBotResponse(Userinput);
+        },600 );
+
+
+
+};
+
+const renderChatBotResponse = (Userinput) => {
+        const res = getChatBotResponse(Userinput)
+        renderMessageElement(res);
+};
+
+const renderMessageElement = (txt, type) => {
+        let className = "user-message";
+        if(type!== 'user'){
+        className="chatbot-message";
+}
+        const chatBody = document.getElementById('chat-body');
+        const messageElement = document.createElement('div')
+        const txtnode = document.createTextNode(txt)
+        messageElement.classList.add(className);
+        messageElement.appendChild(txtnode)
+        chatBody.appendChild(messageElement)
+
+}
+const getChatBotResponse = (Userinput) => {
+        return responseObj[Userinput] == undefined ? "Por facor tente novamente" : responseObj[Userinput]
+}
+
+const Minimize = () => {
+
+const elementoParaEsconder = document.getElementById('Neymar');
+
+
+elementoParaEsconder.style.display = 'none';
+}
+
